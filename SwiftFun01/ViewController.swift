f1eb66d30edf2276da1da80e64d0ab7bba5edbbc
Hai Feng <haifeng@cocoaspice.in>
2019-07-11 15:54:48 +0800

    initial commit

---
+++ SwiftFun01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftFun01
+//
+//  Created by lono on 2019/7/11.
+//  Copyright © 2019 lono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
